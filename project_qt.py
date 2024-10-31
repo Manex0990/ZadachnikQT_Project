870ed9c69d21d1626d9795569818b8ab9235db68
Manex0990 <168935408+Manex0990@users.noreply.github.com>
2024-10-31 13:55:01 +0300

    project_qt.py

--- project_qt.py
+++ project_qt.py
@@ -4,18 +4,25 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
-class Titul_list(QMainWindow):
+class Menu(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('zadachnik.ui', self)
-        self.start_btn.clicked.connect(self.test_window)
-
-    def test_window(self):
-        pass
+        uic.loadUi('zadachnik_menu.ui', self)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Titul_list()
+    ex = Menu()
     ex.show()
     sys.exit(app.exec())
+
+
+# square_x_btn
+# line_x_btn
+# sum_btn
+# min_btn
+# mul_btn
+# crop_btn
+# easy_test_btn
+# hard_test_btn
+# добавить картинки
